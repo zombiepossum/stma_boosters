8eb09e7d8da730882ac85a353b08feb47c7a463e
Joe Muraski <jmuraski@shipt.com>
2024-04-16 14:47:52 -0500

    updated with roster

---
+++ scripts/createVarsityCards.py
@@ -0,0 +1,16 @@
+from jinja2 import Environment, FileSystemLoader
+import csv
+
+with open('./rosters/2024-04-16-roster.csv', 'r') as file:
+    csv_reader = csv.DictReader(file)
+    players = [row for row in csv_reader]
+
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("varsity_card_template.txt")
+
+for player in players:
+  if player['team'] == 'Varsity':
+    content = template.render(player)
+    with open('./varsity-roster-cards.html', mode="a", encoding="utf-8") as message:
+        message.write(content)
